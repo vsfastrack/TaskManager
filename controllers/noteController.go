157ad63685ef492da953bad1a8475df5c171dc62
vaibhav <vaibhav@fintechlabs.in>
2018-07-26 18:06:47 +0530

    Changed the note controller

--- controllers/noteController.go
+++ controllers/noteController.go
@@ -1,45 +1,45 @@
-package controllers
+// package controllers
 
-import (
-	"encoding/json"
-	"net/http"
+// import (
+// 	"encoding/json"
+// 	"net/http"
 
-	"github.com/vsfastrack/TaskManager/common"
-	"github.com/vsfastrack/TaskManager/data"
-)
+// 	"github.com/vsfastrack/TaskManager/common"
+// 	"github.com/vsfastrack/TaskManager/data"
+// )
 
-//CreateNote handler
-func CreateNote(w http.ResponseWriter, r *http.Request) {
+// //CreateNote handler
+// func CreateNote(w http.ResponseWriter, r *http.Request) {
 
-	var noteResource NoteResource
+// 	var noteResource NoteResource
 
-	err := json.NewDecoder(r.Body).Decode(&noteResource)
+// 	err := json.NewDecoder(r.Body).Decode(&noteResource)
 
-	if err != nil {
-		common.DisplayAppError(w, err, "Invalid Note Data", 500)
-		return
-	}
+// 	if err != nil {
+// 		common.DisplayAppError(w, err, "Invalid Note Data", 500)
+// 		return
+// 	}
 
-	context := NewContext()
-	defer context.Close()
-	c := context.DbCollection("notes")
+// 	context := NewContext()
+// 	defer context.Close()
+// 	c := context.DbCollection("notes")
 
-	repo := &data.
-	note := &noteResource.Data
+// 	repo := &data.NoteRepository{c}
+// 	note := &noteResource.Data
 
-	repo.Create(noteResource)
-	if j, err := json.Marshal(TaskResource{Data: *note}); err != nil {
-		common.DisplayAppError(
-			w,
-			err,
-			"An unexpected error has occurred",
-			500,
-		)
-		return
-	} else {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusCreated)
-		w.Write(j)
-	}
+// 	repo.Create(noteResource)
+// 	if j, err := json.Marshal(TaskResource{Data: *note}); err != nil {
+// 		common.DisplayAppError(
+// 			w,
+// 			err,
+// 			"An unexpected error has occurred",
+// 			500,
+// 		)
+// 		return
+// 	} else {
+// 		w.Header().Set("Content-Type", "application/json")
+// 		w.WriteHeader(http.StatusCreated)
+// 		w.Write(j)
+// 	}
 
-}
+// }
